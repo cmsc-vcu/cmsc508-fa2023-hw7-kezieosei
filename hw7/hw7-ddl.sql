7296a03caf84dbbcffc297bc41e128b7fd696b35
kezieosei <oseika@vcu.edu>
2023-12-11 23:20:26 -0500

    my changes

--- hw7/hw7-ddl.sql
+++ hw7/hw7-ddl.sql
@@ -18,6 +18,7 @@
 
 SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS skills;
 # ... 
 SET FOREIGN_KEY_CHECKS=1;
 
@@ -28,11 +29,33 @@ SET FOREIGN_KEY_CHECKS=1;
 # time committment offers some sense of how much time was required (or will be required) to gain the skill.
 # You can assign the skill descriptions.  Please be creative!
 
+CREATE TABLE skills (
+    skill_id int NOT NULL,
+    skill_name varchar(256) NOT NULL,
+    skill_description TEXT NOT NULL,
+    skill_tag varchar(256) NOT NULL,
+    skill_url varchar(256),
+    skill_timecommit int,
+    PRIMARY KEY (skill_id)
+);
+
+
 
 # Section 3
 # Populate skills
 # Populates the skills table with eight skills, their tag fields must exactly contain “Skill 1”, “Skill 2”, etc.
 # You can assign skill names.  Please be creative!
+
+
+INSERT INTO skills(skill_id, skill_name, skill_description, skill_tag, skill_url, skill_timecommit) VALUES
+(1, "Juggling", "Throwing things up and not letting them fall. Warning: May involve throwing bananas.", "juggling", "juggling.com", 3),
+(2, "Tightrope Walking", "Balancing on a thin line high above the ground without freaking out. Safety net not included.", "tightrope", "tightrope.com", 4),
+(3, "Clowning", "Making people laugh without saying a word. Red nose and oversized shoes optional but highly recommended.", "clowning", "clowninglol.com", 5),
+(4, "Aerial Silk Dancing", "Twirling gracefully in the air while wrapped in beautiful fabrics. May cause serious envy in birds.", "aerial-silk", "aerial.com", 4),
+(5, "Fire Breathing", "Extinguishing birthday candles with style. Warning: Don't try this at home, or anywhere for that matter.", "fire-breathing", "firebreathingg.com", 2),
+(6, "Acrobatics", "Doing flips, spins, and twists without turning into a human pretzel. Spandex not mandatory, but preferred.", "acrobatics", "acrobatics.com", 4),
+(7, "Hula Hooping", "Swirling hoops around various body parts like it's the world's most stylish exercise routine. Hipster sunglasses recommended.", "hula-hooping", "hulahula.com", 3),
+(8, "Human Cannonball", "Being launched through the air in a giant cannon. Helmet and an adventurous spirit required. Potential for superhero status.", "cannonball", "cannonballwoooo.com", 6);
 
 
 # Section 4
@@ -41,8 +64,15 @@ SET FOREIGN_KEY_CHECKS=1;
 # All other fields can default to NULL.
 
 CREATE TABLE people (
-    people_id int,
+    people_id int NOT NULL,
     people_last_name varchar(256) NOT NULL,
+    people_first_name varchar(256),
+    people_email varchar(256),
+    people_linkedin_url varchar(256),
+    people_headshot_url varchar(256),
+    people_discord_handle varchar(256),
+    people_brief_bio varchar(256), 
+    people_date_joined DATETIME NOT NULL, 
     PRIMARY KEY (people_id)
 );
 
@@ -51,12 +81,33 @@ CREATE TABLE people (
 # Their last names must exactly be “Person 1”, “Person 2”, etc.
 # Other fields are for you to assign.
 
-insert into people (people_id,people_last_name) values (1,'Person 1');
+-- insert into people (people_id,people_last_name) values (1,'Person 1'), (2,'Person 2');
+
+
+INSERT INTO people (people_id, people_last_name, people_first_name, people_email, people_linkedin_url, people_headshot_url, people_discord_handle, people_brief_bio, people_date_joined)
+VALUES 
+(1, 'Person 1', 'Alice', 'alice@email.com', 'linkedin.com/alice', 'headshots.com/alice', 'Alice#1234', 'I love coding and hiking!', NOW()),
+(2, 'Person 2', 'Bob', 'bob@email.com', 'linkedin.com/bob', 'headshots.com/bob', 'Bob#5678', 'Passionate about music and cooking!', NOW()),
+(3, 'Person 3', 'Charlie', 'charlie@email.com', 'linkedin.com/charlie', 'headshots.com/charlie', 'Charlie#9101', 'Tech enthusiast and coffee lover!', NOW()),
+(4, 'Person 4', 'Diana', 'diana@email.com', 'linkedin.com/diana', 'headshots.com/diana', 'Diana#2345', 'Art and travel make life worthwhile!', NOW()),
+(5, 'Person 5', 'Eve', 'eve@email.com', 'linkedin.com/eve', 'headshots.com/eve', 'Eve#6789', 'Fitness freak and bookworm!', NOW()),
+(6, 'Person 6', 'Frank', 'frank@email.com', 'linkedin.com/frank', 'headshots.com/frank', 'Frank#3456', 'Film buff and amateur chef!', NOW());
+
 
 
 # Section 6
 # Create peopleskills( id, skills_id, people_id, date_acquired )
 # None of the fields can ba NULL. ID can be auto_increment.
+
+CREATE TABLE peopleskills (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    skills_id INT NOT NULL,
+    people_id INT NOT NULL,
+    date_acquired DATETIME NOT NULL,
+    FOREIGN KEY (skills_id) REFERENCES skills(skill_id),
+    FOREIGN KEY (people_id) REFERENCES people(people_id)
+);
+
 
 
 # Section 7
@@ -72,11 +123,50 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Person 9 has skills 2,5,6;
 # Person 10 has skills 1,4,5;
 # Note that no one has yet acquired skills 7 and 8.
+
+-- Inserting records for people and their associated skills
+INSERT INTO peopleskills (skills_id, people_id, date_acquired) VALUES
+-- Person 1
+(1, 1, NOW()), (3, 1, NOW()), (6, 1, NOW()),
+
+-- Person 2
+(3, 2, NOW()), (4, 2, NOW()), (5, 2, NOW()),
+
+-- Person 3
+(1, 3, NOW()), (5, 3, NOW()),
+
+-- Person 4 (No skills)
+
+-- Person 5
+(3, 5, NOW()), (6, 5, NOW()),
+
+-- Person 6
+(2, 6, NOW()), (3, 6, NOW()), (4, 6, NOW()),
+
+-- Person 7
+(3, 7, NOW()), (5, 7, NOW()), (6, 7, NOW()),
+
+-- Person 8
+(1, 8, NOW()), (3, 8, NOW()), (5, 8, NOW()), (6, 8, NOW()),
+
+-- Person 9
+(2, 9, NOW()), (5, 9, NOW()), (6, 9, NOW()),
+
+-- Person 10
+(1, 10, NOW()), (4, 10, NOW()), (5, 10, NOW());
+
  
 
 # Section 8
 # Create roles( id, name, sort_priority )
 # sort_priority is an integer and is used to provide an order for sorting roles
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    sort_priority INT NOT NULL
+);
+
 
 
 
@@ -85,12 +175,27 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Designer, Developer, Recruit, Team Lead, Boss, Mentor
 # Sort priority is assigned numerically in the order listed above (Designer=10, Developer=20, Recruit=30, etc.)
 
+INSERT INTO roles (name, sort_priority) VALUES
+('Designer', 10),
+('Developer', 20),
+('Recruit', 30),
+('Team Lead', 40),
+('Boss', 50),
+('Mentor', 60);
 
 
 # Section 10
 # Create peopleroles( id, people_id, role_id, date_assigned )
 # None of the fields can be null.  ID can be auto_increment
 
+CREATE TABLE peopleroles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    people_id INT NOT NULL,
+    role_id INT NOT NULL,
+    date_assigned DATETIME NOT NULL,
+    FOREIGN KEY (people_id) REFERENCES people(people_id),
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
 
 
 # Section 11
@@ -106,3 +211,23 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Person 9 is Developer
 # Person 10 is Developer and Designer
 
+-- Inserting records for role assignments
+INSERT INTO peopleroles (people_id, role_id, date_assigned) VALUES
+(1, 2, NOW()), -- Person 1 is a Developer
+(2, 5, NOW()), -- Person 2 is a Boss
+(2, 6, NOW()), -- Person 2 is also a Mentor
+(3, 2, NOW()), -- Person 3 is a Developer
+(3, 4, NOW()), -- Person 3 is also a Team Lead
+(4, 3, NOW()), -- Person 4 is a Recruit
+(5, 3, NOW()), -- Person 5 is a Recruit
+(6, 2, NOW()), -- Person 6 is a Developer
+(6, 1, NOW()), -- Person 6 is also a Designer
+(7, 1, NOW()), -- Person 7 is a Designer
+(8, 1, NOW()), -- Person 8 is a Designer
+(8, 4, NOW()), -- Person 8 is also a Team Lead
+(9, 2, NOW()), -- Person 9 is a Developer
+(10, 2, NOW()), -- Person 10 is a Developer
+(10, 1, NOW()); -- Person 10 is also a Designer
+
+
+select * FROM skills;
